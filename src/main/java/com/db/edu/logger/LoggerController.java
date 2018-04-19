65526918ca48ff101ce47c628e5416eb98d546af
noobiland <gillinger8@gmail.com>
2018-04-19 11:47:44 +0300

    add logger project

---
+++ src/main/java/com/db/edu/logger/LoggerController.java
@@ -0,0 +1,11 @@
+package com.db.edu.logger;
+
+public class LoggerController {
+    private LoggerSaver saver = new LoggerSaver();
+    /**
+     * @param message
+     */
+    public void log(String message){
+        saver.save(message);
+    }
+}
---
+++ src/test/java/com/db/edu/LoggerIT.java
@@ -0,0 +1,10 @@
+package com.db.edu;
+
+import com.db.edu.logger.LoggerController;
+
+public class LoggerIT {
+    public static void main(String[] args) {
+        LoggerController logger = new LoggerController();
+        logger.log("test message!!11");
+    }
+}
