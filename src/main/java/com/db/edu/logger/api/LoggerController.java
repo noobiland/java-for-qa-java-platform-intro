78cd623a40e1e84b5b7bab8ccea8b2ac6446fcb6
noobiland <gillinger8@gmail.com>
2018-04-23 12:04:42 +0300

    add training enterprise structure

---
+++ src/main/java/com/db/edu/logger/api/LoggerController.java
@@ -0,0 +1,23 @@
+package com.db.edu.logger.api;
+
+import com.db.edu.logger.businesslogic.service.LogService;
+
+/**
+ * Controller's responsibilities:
+ * - Security
+ * - Logging
+ * - TXs
+ * - Request preparation
+ * - Routing
+ * - other Aspects
+ */
+public class LoggerController {
+    private LogService logService = new LogService();
+    /**
+     * @param message to be logged
+     *
+     */
+    public void log(String message){
+        logService.log(message);
+    }
+}
---
+++ src/main/java/com/db/edu/logger/businesslogic/service/LogMessageFilter.java
@@ -0,0 +1,7 @@
+package com.db.edu.logger.businesslogic.service;
+
+public class LogMessageFilter {
+    public boolean filter(String message) {
+        return true;
+    }
+}
